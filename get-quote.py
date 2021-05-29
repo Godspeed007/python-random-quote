dc29da8c8479142d754f2d240f6037af6373e103
Abhijeet Singh <50884193+Godspeed007@users.noreply.github.com>
2021-05-29 21:24:51 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def patron():
+def main():
    print("Keep it logically awesome.")
 
   f = open("quotes.txt")
@@ -7,4 +7,4 @@ def patron():
 
   print(quotes[3])
 if __name__== "__main__":
-  patron()
+  main()
